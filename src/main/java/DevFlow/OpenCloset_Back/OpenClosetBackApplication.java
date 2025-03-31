cf124f350e2add10631075fc12ff0e739f8d10b8
oxoxoxoxox <dudwlsaos132@naver.com>
2025-03-31 16:23:24 +0900

    Initial commit

---
+++ src/main/java/DevFlow/OpenCloset_Back/OpenClosetBackApplication.java
@@ -0,0 +1,13 @@
+package DevFlow.OpenCloset_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenClosetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenClosetBackApplication.class, args);
+	}
+
+}
