0772b547db89a14addaee3b9e5d7891142a1e495
oxoxoxoxox <dudwlsaos132@naver.com>
2025-04-02 17:11:12 +0900

    [add] 게시판 생성 request Dto 파일 생성

--- src/main/java/DevFlow/OpenCloset_Back/entity/Board.java
+++ src/main/java/DevFlow/OpenCloset_Back/entity/Board.java
@@ -20,7 +20,16 @@ public class Board extends Default{ //게시물 id
     private String Description; //의류에 관한 설명
 
     @Column(nullable = false)
-    private String image;   //의류 사진 (url로 단순 db 삽이 예정)
+    private String image;   //의류 사진 (url로 단순 db 삽입 예정)
+
+    @Column(nullable = false)
+    private String size;    //의류 사이즈
+
+    @Column(nullable = false)
+    private Boolean sex;    //해당 의류 성별(1 : 남자, 0 : 여자)
+
+    @Column(nullable = false)
+    private String place;   //거래 장소
 
     @CreatedDate
     private LocalDateTime createdAt;    //엔티티가 생성되어 저장될 때 시간을 자동 저장
@@ -28,5 +37,6 @@ public class Board extends Default{ //게시물 id
     @LastModifiedDate
     private LocalDateTime modifiedAt;   //엔티티가 수정된 시간을 자동 저장
 
-    //회원아이디에 해당하는 FK값도 넣어야함 (회원 로직 구현 시)
+    //회원아이디에 해당하는 FK값도 넣어야함. (회원 로직 구현 시)
+    //카테고리에 해당하는 FK값 넣어야함. (아마도..?)
 }
---
+++ src/main/java/dto/req/BoardCreateRequestDto.java
@@ -0,0 +1,10 @@
+package dto.req;
+
+public class BoardCreateRequestDto {
+    private String title;
+    private String Description;
+    private String image;
+    private String size;
+    private Boolean sex;
+    private String place;
+}
