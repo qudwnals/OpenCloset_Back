7a6fc6c9f15b70923cceb814a24ce6bf0f04dda4
qudwnals <163666385+qudwnals@users.noreply.github.com>
2025-04-11 00:44:42 +0900

    불 필요 코드가 아니였음 ㅅㅂ

--- src/main/java/DevFlow/OpenCloset_Back/Board/Repository/TopRepository.java
+++
@@ -1,9 +0,0 @@
-package DevFlow.OpenCloset_Back.Board.Repository;
-
-import DevFlow.OpenCloset_Back.Board.entity.Top;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TopRepository extends JpaRepository<Top, Long> {
-}
