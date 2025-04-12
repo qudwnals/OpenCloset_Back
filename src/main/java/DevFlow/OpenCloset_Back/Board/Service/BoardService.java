eee17ded7dfce6039385dfbf1f7ac9da7f281587
oxoxoxoxox <dudwlsaos132@naver.com>
2025-04-12 23:04:54 +0900

    [add] 해당 카테고리가 bottom이면 bottom테이블에 저장

--- src/main/java/DevFlow/OpenCloset_Back/Board/Service/BoardService.java
+++ src/main/java/DevFlow/OpenCloset_Back/Board/Service/BoardService.java
@@ -2,11 +2,13 @@ package DevFlow.OpenCloset_Back.Board.Service;
 
 import DevFlow.OpenCloset_Back.Board.Repository.BoardRepository;
 import DevFlow.OpenCloset_Back.Board.Repository.BottomRepository;
+import DevFlow.OpenCloset_Back.Board.Repository.OuterRepository;
 import DevFlow.OpenCloset_Back.Board.Repository.TopRepository;
 import DevFlow.OpenCloset_Back.Board.dto.req.BoardCreateRequestDto;
 import DevFlow.OpenCloset_Back.Board.dto.res.BoardCreateResponsetDto;
 import DevFlow.OpenCloset_Back.Board.entity.Board;
 import DevFlow.OpenCloset_Back.Board.entity.Bottom;
+import DevFlow.OpenCloset_Back.Board.entity.Outer;
 import DevFlow.OpenCloset_Back.Board.entity.Top;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,11 +21,13 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final TopRepository topRepository;
     private final BottomRepository bottomRepository;
+    private final OuterRepository outerRepository;
 
-    public BoardService(BoardRepository boardRepository, TopRepository topRepository, BottomRepository bottomRepository) {
+    public BoardService(BoardRepository boardRepository, TopRepository topRepository, BottomRepository bottomRepository, OuterRepository outerRepository) {
         this.boardRepository = boardRepository;
         this.topRepository = topRepository;
         this.bottomRepository = bottomRepository;
+        this.outerRepository = outerRepository;
     }
 
     @Transactional(readOnly = true)
@@ -36,14 +40,19 @@ public class BoardService {
         Board board = new Board(req);
         boardRepository.save(board);
 
-        if (req.getCategory().equals("top")){
+        if (req.getCategory().equals("top")){   //상의
             Top top = new Top(board);
             topRepository.save(top);
         }
-        if (req.getCategory().equals("bottom")){
+        if (req.getCategory().equals("bottom")){    //하의
             Bottom bottom = new Bottom(board);
             bottomRepository.save(bottom);
         }
+        if (req.getCategory().equals("outer")){ //outer
+            Outer outer = new Outer(board);
+            outerRepository.save(outer);
+        }
+        
         return new BoardCreateResponsetDto(board);
     }
     @Transactional
